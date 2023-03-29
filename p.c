7b3cf2c149b8f61e748421579f468c2dbb8ef0e0
Fernando Dodino <fernando.dodino@gmail.com>
2023-03-28 22:44:02 -0300

    adding output to unexpressive p function

--- p.c
+++ p.c
@@ -14,7 +14,11 @@ void coso(int r[], int a[]) {
 int main() {
     int x[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int y[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    coso(x,y);
+    coso(x, y);
+    for (int i = 0; i < 10; i++) {
+        printf(" %d ", y[i]);
+    }
+    printf("\n");
     return 0;
 }
 
