54198a63d42ab2480a33aee050358bd38cc2b20a
Fernando Dodino <fernando.dodino@gmail.com>
2023-03-28 22:39:51 -0300

    adding unexpressive solution

---
+++ p.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// primera variante
+
+void coso(int r[], int a[]) {
+    int j = 0;
+    for (int i = 0; i < 10; i++) {
+        if (r[i] % 2 == 0) {
+            a[j++] = r[i];
+        }
+    }
+}
+
+int main() {
+    int x[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int y[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    coso(x,y);
+    return 0;
+}
+
