aa66302d3355cfedd48316e10a0d6fd373fa67e6
Juancete <juan.contardo@gmail.com>
2023-03-29 14:25:06 -0300

    paso a lógica de punteros

--- pares.c
+++ pares.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void filtrarPares(int numeros[], int pares[]) {
-    int j = 0;
+int filtrarPares(int numeros[], int** pares) {
+    int cantidad = 0;
     for (int i = 0; i < 10; i++) {
         if (numeros[i] % 2 == 0) {
-            pares[j] = numeros[i];
-            j++;
+            cantidad = cantidad + 1;
+            if (cantidad == 1) {
+              * pares = (int*)malloc(sizeof(int));
+            }
+            else{
+              * pares = (int *)realloc(*pares, sizeof(int)*cantidad);
+            }
+            * (*pares + cantidad -1)  = numeros[i];
         }
     }
+    return cantidad;
 }
 
 int main() {
     int numeros[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int pares[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    filtrarPares(numeros, pares);
+    int* pares;
+    int largoPares;
+    largoPares = filtrarPares(numeros, &pares);
     printf("Los números pares son: ");  
-    for (int i = 0; i < 10; i++) {
-        printf(" %d ", pares[i]);
+    for (int i = 0; i < largoPares; i++) {
+        printf(" %d ", *(pares + i));
     }
     printf("\n");
     return 0;
 }
-
